4fdb02f184ecadb5393701ab4a3ae975bccda500
Tien Nguyen Minh <tien3296@gmail.com>
2023-06-06 14:48:36 +0700

    test event

--- support/bash/bash-shellcheck.sh
+++ support/bash/bash-shellcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git diff --name-only $CIRCLE_SHA1 > list
 cat list
-while IFS= read -r line;
-do
-	shellcheck IFS
-done < list
+# while IFS= read -r line;
+# do
+# 	shellcheck IFS
+# done < list
